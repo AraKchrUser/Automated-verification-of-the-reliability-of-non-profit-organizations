b124c9d11d5dfeaa33f375f7ad9a8fb47b97395a
AraKchrUser <armen0101017@gmail.com>
2021-08-26 16:20:17 +1000

    Add make dataset

---
+++ src/make_dataset.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+if __name__ == "__main__":
+
+    # Данные о ссылках на сайтах и о фонде: подборбнее к  mmvvrr
+    data = pd.read_csv('data/refer_in_site.csv')
+
+    # Обработка
+    data['ИНН'] = data['ИНН'].apply(lambda x: 0 if x == 'nan' else 1)
+    data['Благонадежный (да/нет)'] = data['Благонадежный (да/нет)'].map({'да': 1, 'нет': 0})
+    data['Маил Добро'] = data['Маил Добро'].apply(lambda x: 0 if x == pd.NaT else 1)
+    data['Нужна помощь'] = data['Нужна помощь'].apply(lambda x: 0 if x == pd.NaT else 1)
+    data['Нужна помощь Фонд'] = data['Нужна помощь Ссылка'].astype(str).apply(lambda x: 0 if x == 'nan' else float(x))
+    df_preprocess = data[['Наименование', 'Город', 'ИНН', 'Благонадежный (да/нет)', 'Маил Добро', 'Нужна помощь',
+                         'Нужна помощь Фонд']].copy()
+
+    # Файл docker содержит данные - результаты обраотки новостей, отзывов  т. д.
+    news_sentimental = pd.read_csv('../docker/save_file/news_summarization.csv')
+    # merge
