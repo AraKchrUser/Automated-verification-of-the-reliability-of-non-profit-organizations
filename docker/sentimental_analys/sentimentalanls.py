3432ecca70c23ca337c1fa9e3fb73d36ceddb249
AraKchrUser <armen0101017@gmail.com>
2021-08-29 22:42:51 +1000

    docker file tuned

--- docker/sentimental_analys/sentimentalanls.py
+++ docker/sentimental_analys/sentimentalanls.py
@@ -1,7 +1,8 @@
 from deeppavlov import build_model, configs
 
 print('1 ---> ')
-model = build_model(configs.classifiers.rusentiment_elmo_twitter_cnn, download=True)
+# download=False если данные скачваться не будут
+model = build_model(configs.classifiers.rusentiment_elmo_twitter_cnn, download=False)
 print('2 ---> ')
-with open('/save_file/sentimental_result.txt') as f:
+with open('/save_file/sentimental_result.txt', 'w') as f:
     f.write(model(['В Африке воды мало, но она вкусная'])[0])
