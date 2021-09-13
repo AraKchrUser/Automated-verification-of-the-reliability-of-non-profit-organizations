ec71d6b09c0eafc2f301962c6f4910bc6080be4d
AraKchrUser <armen0101017@gmail.com>
2021-09-13 18:12:41 +1000

    flask server add

---
+++ docker/flask_server/server.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, url_for, request, render_template, redirect
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+
+
+@app.route('/', methods=['POST', 'GET'])
+def form_sample():
+    """Форма, где пользователь вводит значения..."""
+
+    if request.method == 'GET':
+        pass
+        # return render_template('index.html', style_path=url_for('static', filename='css/style.css'),
+        #                        text=data.get_text(), question=data.get_question(), answer=data.get_answer(),
+        #                        user_name=user_name)
+    elif request.method == 'POST':
+        pass
+        # if request.form.get('accept'):
+        #     pass
+        # # Получить ответ на вопрос
+        # return render_template('index.html', style_path=url_for('static', filename='css/style.css'),
+        #                        text=data.get_text(), question=data.get_question(), answer=data.get_answer(),
+        #                        user_name=user_name)
+    return render_template('index.html')
+
+
+def main():
+    app.run(port=8080, host='127.0.0.1')
+
+
+if __name__ == '__main__':
+    main()
